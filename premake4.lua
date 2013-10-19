669e5aadf30e6cac1e249925517f2c172188a8a8
Dmitry Ledentsov <dmlled@yahoo.com>
2013-10-19 23:55:11 +0200

    initial commit

---
+++ premake4.lua
@@ -0,0 +1,29 @@
+_G.package.path=_G.package.path..[[;./?.lua;./?/?.lua]]
+assert ( require 'premake.quickstart' )
+
+make_solution 'lua-sophia'
+
+includedirs { 
+	'./sophia/db'
+}
+
+make_shared_lib( 'sophia',
+	{
+		'./sophia/db/*.h',
+		'./sophia/db/*.c',
+		'./lua-sophia/lua-sophia.c'
+	}
+)
+language "C"
+targetprefix ''
+targetdir '.'
+
+-- links
+configuration 'macosx'
+	links { 'lua' }
+	targetextension '.so'
+configuration 'linux'
+	links { 'lua5.1-c++' }
+configuration 'windows'
+	links { "lua5.1" }
+configuration { '*' }
